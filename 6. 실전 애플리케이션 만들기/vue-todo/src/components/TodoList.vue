f1fa236236a4838e5cc0c2d904df5b6c04f20848
busungg <busungg@naver.com>
2022-03-01 23:13:24 +0900

    Updated 실전 애플리케이션 만들기

--- 6. 실전 애플리케이션 만들기/vue-todo/src/components/TodoList.vue
+++ 6. 실전 애플리케이션 만들기/vue-todo/src/components/TodoList.vue
@@ -1,9 +1,72 @@
 <template>
-  <div>footer</div>
+  <section>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" class="shadow">
+        <!-- index는 v-for 디렉티브에서 기본적으로 제공하는 변수 입니다. -->
+        <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+        {{ todoItem }}
+        <span
+          class="removeBtn"
+          type="button"
+          @click="removeTodo(todoItem, index)"
+        >
+          <i class="far fa-trash-alt" aira-hidden="true"></i>
+        </span>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      todoItems: []
+    };
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+  methods: {
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  }
+};
 </script>
 
-<style></style>
+<style>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>
